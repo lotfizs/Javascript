99434d551cec0edb901fbda2b6086c1efbd9daf3
lzsnation <2133349@talnet.com>
2020-12-03 13:09:04 +0100

    h02

---
+++ h02/Op2.1/vulmetjes.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subknop").innerText = "Mijn geweldige subknop";
+document.getElementById("aap1").src = "img/aap1.jpg";
+document.getElementById("aap2").src = "img/aap4.jpg";
